33010b427c06d0f395e90d950db887438c60372b
Pablo Andres <p9andres@gmail.com>
2021-04-16 18:25:54 +0200

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("HOla, mundo");
+import dayjs from "./node_modules/dayjs";
+
+const fechaHoy = dayjs().format('DD/MM/YYYY');
+const parrafoFecha = document.getElementById("parrafoFecha");
+
+parrafoFecha.textContent = fechaHoy;
+console.log(parrafoFecha);
+
+
+const modalPlato = document.getElementById('modal');
+const imgPlato = document.getElementById('plato');
+const btnCerrar = document.getElementById('btn-cerrar');
+
+btnCerrar.addEventListener('click', function (event) {
+    modalPlato.style.display = 'none';
+});
+
+imgPlato.addEventListener('click', function (event) {
+    modalPlato.style.display = 'block';
+});
